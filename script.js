eb35dd450df5923ce30e255a726ba9dc888a4b39
NatsukiSh <108126196+NatsukiSh@users.noreply.github.com>
2024-01-18 21:51:34 +0100

    Added endGameText const

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const grid = document.querySelector(".grid");
 const stackBtn = document.querySelector(".stack");
 const scoreCounter = document.querySelector(".score-counter");
 const endGameScreen = document.querySelector(".end-game-screen");
+const endGameText = document.querySelector(".end-game-text");
 const playAgainBtn = document.querySelector(".play-again");
 
 //Create game grid matrix
